8bc082c86bd907477dd38bd7a56ae5ae6321d9bf
SaveliyFrolikov <thecoder13oops@gmail.com>
2022-02-28 19:30:42 +0200

    Window code

---
+++ Labyrinth.py
@@ -0,0 +1,11 @@
+from pygame import *
+
+background_colour = (255,255,255)
+width = 200
+height = 300
+screen = display.set_mode((width, height))
+display.set_caption('Labyrinth')
+screen.fill(background_colour)
+display.flip()
+
+

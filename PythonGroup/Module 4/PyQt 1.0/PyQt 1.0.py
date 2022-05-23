7be3d4fe7356bf586ad83d37c0b33f6f9040b815
bakiblond <lolergamingo@gmail.com>
2022-05-23 19:24:59 +0300

    Update PyQt 1.0.py

--- PythonGroup/Module 4/PyQt 1.0/PyQt 1.0.py
+++ PythonGroup/Module 4/PyQt 1.0/PyQt 1.0.py
@@ -1,1 +1,14 @@
 # https://docs.google.com/presentation/d/1gqyMy49e3D3A0E9ACEwZGh825fAHHC7Q8yT97-nw4Qw/edit#slide=id.g12dae56b7f6_0_222
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication,QMainWidow
+import sys
+
+
+class MyWindow(QMainWidow):
+    def __init__(self):
+
+
+    def CreateLabel()
+        
+app = QApplication([])
+sys.exit(app.exec_())
